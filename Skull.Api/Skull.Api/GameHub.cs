2cd9e7ba3b0de7ebe52ea14bdd090f0d4ea65168
Dan <beta.minus@gmail.com>
2022-08-30 21:06:47 -0500

    SignalR

---
+++ Skull.Api/Skull.Api/GameHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Skull.Api
+{
+    public class GameHub : Hub
+    {
+        public async Task SendMessage(string game, string user, string message)
+        {
+            await Clients.Clients(new[] { "Booyah" }).SendAsync("ReceiveMessage", message);
+        }
+    }
+}
